289d286d0cd41d184ac5a46f6b60a4426f2bcd30
scherida rayane <scheridarayane@gmail.com>
2021-08-31 13:43:51 -0300

    first commit

---
+++ app/src/main/java/com/example/atividade_nv3/DetalhesActivity.java
@@ -0,0 +1,65 @@
+package com.example.atividade_nv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.atividade_nv3.model.Funcionario;
+import com.example.atividade_nv3.model.FuncionarioDAO;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class DetalhesActivity extends AppCompatActivity {
+
+    TextView tvNomeFuncionario;
+    TextView tvSalarioLiquido;
+    TextView tvSalarioBruto;
+    TextView tvINSS;
+    TextView tvIR;
+    TextView tvFGTS;
+    Button bVoltar;
+    FuncionarioDAO dao;
+    NumberFormat numberFormat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+        declarations();
+        dao = new FuncionarioDAO();
+        numberFormat = NumberFormat.getInstance(new Locale("pt", "BR"));
+        Intent intent = getIntent();
+
+        int position = intent.getIntExtra("position", -1);
+        if (position == -1) { finish(); }
+
+        Funcionario funcionario = dao.getFuncionario(position);
+
+        tvNomeFuncionario.setText(funcionario.getNome());
+        tvSalarioBruto.setText("Salário bruto: " + funcionario.getSalarioBruto());
+        tvFGTS.setText("FTGS: " + funcionario.getFgts());
+        tvINSS.setText("INSS: " + funcionario.getInss());
+        tvIR.setText("IR: " + funcionario.getIr());
+        tvSalarioLiquido.setText("Salário Líquido: " + funcionario.getSalarioLiquido());
+
+        bVoltar.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+
+    private void declarations() {
+        tvNomeFuncionario = findViewById(R.id.tvNomeFuncionario);
+        tvSalarioBruto = findViewById(R.id.tvSalarioBruto);
+        tvFGTS = findViewById(R.id.tvFGTS);
+        tvIR = findViewById(R.id.tvIR);
+        tvINSS = findViewById(R.id.tvINSS);
+        tvSalarioLiquido = findViewById(R.id.tvSalarioLiquido);
+
+        bVoltar = findViewById(R.id.bVoltar);
+    }
+}
---
+++ app/src/main/java/com/example/atividade_nv3/model/Funcionario.java
@@ -0,0 +1,91 @@
+package com.example.atividade_nv3.model;
+
+public class Funcionario {
+
+    private String nome;
+    private double horasTrabalhadas;
+    private double valorHora;
+    private double salarioBruto;
+    private double salarioLiquido;
+    private double inss;
+    private double fgts;
+    private double ir;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, double horasTrabalhadas, double valorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+        this.salarioBruto = setSalarioBruto(valorHora, horasTrabalhadas);
+        this.inss = setInss(this.salarioBruto);
+        this.fgts = setFgts(this.salarioBruto);
+        this.ir = setIr(this.salarioBruto);
+        this.salarioLiquido = getSalarioBruto() - (getInss() + getIr());
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+    
+    public double setSalarioBruto(double vhoras, double htrabalhadas) {
+        return vhoras * htrabalhadas;
+    }
+
+    public double getSalarioLiquido() {
+        return salarioLiquido;
+    }
+
+    public double getFgts() {
+        return fgts;
+    }
+
+    public double setFgts(double salario) {
+        return salario * 0.08;
+    }
+
+    public double getIr() {
+        return ir;
+    }
+
+    public double setIr(double salario) {
+        if(salario > 2743.25) {
+            return salario * 0.275;
+        }
+        else if(salario >= 1372.82 && salario <= 2743.25){
+            return salario * 0.15;
+        }
+        else if(salario <= 1372.81) {return 0;}
+        return 0;
+    }
+
+    public double getInss() {
+        return inss;
+    }
+
+    public double setInss(double salario){
+        if(salario <= 868.29){
+            return salario * 0.08;
+        }
+        else if (salario >= 868.30 && salario <= 2894.28) {
+            return salario * 0.09;
+        }
+        else if (salario > 2894.29) {
+            return 318.37;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+
+}
